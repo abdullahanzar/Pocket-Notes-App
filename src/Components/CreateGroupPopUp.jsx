e4b46284b4ba6e21a31422d7e7ae8652be5a132f
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-05-02 21:56:03 +0530

    Side Pane Functionality Implemented

--- src/Components/CreateGroupPopUp.jsx
+++ src/Components/CreateGroupPopUp.jsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect, useRef} from 'react'
 import './CreateGroupPopUp.css'
+import Group from './Group'
 
-export default function CreateGroupPopUp() {
+export default function CreateGroupPopUp(props) {
     const colors = ["#B38BFA", "#FF79F2", "#43E6FC", "#F19576", "#0047FF", "#6691FF"]
     const [chosenColor, setChosenColor] = useState("")
     const [chosenName, setChosenName] = useState("")
@@ -9,21 +10,46 @@ export default function CreateGroupPopUp() {
   return (
     <div className="CG_Everything">
     <div className='CG_PopUp'>
+        <button onClick={()=>props.setExited(false)}>X</button>
         <h3>Create New Notes Group</h3>
         <label htmlFor="GroupName">
-        Group Name: <input type="text" name='GroupName' id='GroupName' placeholder='Enter your group name....'/>
+        Group Name: <input type="text" name='GroupName' id='GroupName' placeholder='Enter your group name....' value={chosenName}
+        onChange={(e)=>{
+            setChosenName(e.target.value)
+        }}/>
         </label>
         <label htmlFor='GroupColor'>Choose Color:
         {
+            (chosenColor=="")? 
             colors.map((item, index) => (
                 <input style={{background:`${item}`}} onClick={()=>{
                     setChosenColor(`${item}`)
                     setDisabled(true)
-                    console.log(chosenColor)
                 }} key={index} type='button' disabled={disabled}/>
              ))
+            :
+            <>
+            <input style={{background: `${chosenColor}`}} type="button" onClick={()=>{
+                    setDisabled(false)
+                    setChosenColor("")
+            }}/>
+            <p style={{color: `${chosenColor}`, fontWeight: 200, position: "relative", left: "0.5rem", cursor: "pointer"}} onClick={()=>{
+                setDisabled(false)
+                setChosenColor("")
+            }}>Chose wrong? Click on me to select again!</p>
+            </> 
         }
         </label>
+        <button onClick={()=>{
+            if(chosenName == '' || chosenColor == '')
+            alert("Please choose a name or a color!") //TODO: Implement a better UI
+            else {
+            localStorage.setItem("Group Name", chosenName)
+            localStorage.setItem("Group Color", chosenColor)
+            props.Implement(chosenName, chosenColor)
+            props.setSelected(false)
+        }
+        }}>Create</button>
     </div>
     </div>
   )
--- src/Components/Pane.jsx
+++ src/Components/Pane.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Group from './Group'
 import CreateGroupPopUp from './CreateGroupPopUp'
 import './Pane.css'
 
 export default function Pane() {
-  const CreateGroup = ()=>{
-    console.log("CreateGroupLogging")
+  const [isCreateGroupSelected, setIsCreateGroupSelected] = useState(false) /*Here isCreateGroupExitedX is used when the user exits without entering the details through the X button*/
+  const [isCreateGroupExitedX, setIsCreateGroupExitedX] = useState(false) /*However if the user enters the info and creates a group isCreateGroupSelected turns to false*/
+  const [Groups, setGroups] = useState(JSON.parse(localStorage.getItem("Groups")))
+
+  const CreateGroup = ()=>(setIsCreateGroupSelected(true), setIsCreateGroupExitedX(true))
+  const ImplementGroup = (chosenName, chosenColor)=>{
+    //let groupInfo = [chosenName, chosenColor]
+    //setGroups(prev=>[prev, groupInfo])
+    setGroups(prev=>({...prev, [chosenName]: {chosenName, chosenColor}}))
+    localStorage.setItem("Groups", JSON.stringify(Groups))
+    console.log(Groups)
   }
 
   return (
@@ -13,7 +22,14 @@ export default function Pane() {
         <h1>Pocket Notes</h1>
         <button onClick={CreateGroup} className='P_CreateNotes'><span style={{fontWeight: 'bold', fontSize: '1.5rem'}}>+</span> &nbsp;&nbsp; Create Notes Group</button>
         <Group color="blue">Cuvette Notes</Group>
-        <CreateGroupPopUp /> 
+        {
+          Object.values(Groups).map(
+            (item, key) => (
+              <Group color={item.chosenColor}>{item.chosenName}</Group>
+            )
+          )
+        }        
+        {isCreateGroupExitedX && isCreateGroupSelected && <CreateGroupPopUp setSelected={setIsCreateGroupSelected} setExited={setIsCreateGroupExitedX} Implement={ImplementGroup}/>}   
     </div>
   )
 }
